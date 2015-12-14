7d3f428924ce862d9bd546222dd91b54fc28d8a4
windreamer <windreamer@gmail.com>
2015-12-14 16:55:59 +0800

    add filename as a default loader option

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const loaderUtils = require('loader-utils');
 export default function (source) {
   this.cacheable && this.cacheable();
   const loaderOptions = loaderUtils.parseQuery(this.query);
-
+  loaderOptions.filename = this.resource;
   const template = jade.compile(source, loaderOptions);
   const requireString = 'var React = require("react");';
   return `${requireString}\nmodule.exports = ${template.toString()}`;
